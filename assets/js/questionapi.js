7296e958031073fc71e2a2f69698b3326734bd8d
Reece <reece@smartit-webdevelopment.com>
2024-02-06 14:17:35 +0000

    Fixed comment indentation.

--- assets/js/questionapi.js
+++ assets/js/questionapi.js
@@ -1,14 +1,16 @@
+// Array of movies for the quiz.
 var movieArray = ["Poor Things", "Saltburn", "The Lord of the Rings: The Fellowship of the Ring", "Django Unchained", "Finding Nemo", "The Beach", "Shutter Island", "Titanic", "Home Alone", "Superbad", "Shrek", "Pinapple Express", "Legally Blonde", "Lara Croft: Tomb Raider", "Good Will Hunting", "Up", "Bee Movie", "Interstellar", "Fight Club", "Fear and Loathing in Las Vegas", "Click", "Kill Bill", "Cast Away", "Blow", "American Psycho", "A Nightmare on Elm Street", "Pulp Fiction", "The Terminator", "The Dark Knight Rises", "Saving Private Ryan", "Toy Story", "Zombieland", "The Gentlemen", "The Conjuring", "Black Hawk Down", "Avatar The Way of Water"];
+
 var giphyApiKey = "nZcsHdIXJYfHdyt5y85wKNj1pmrGVGBh";                                                                                                           // Giphy API Key.
 
 var correctAnswer = [];                                                                                                                                         // Array to hold the correct answer for each question.
-var questionMovieNames = getRandomMovies(movieArray, 4);                                                                                                      // Getting 4 random movies from the movieArray and adding them to a new array.
-var randomIndex = Math.floor(Math.random() * questionMovieNames.length);                                                                                      // Picking a random movie index from the new array.
-var queryMovie = questionMovieNames[randomIndex];                                                                                                             // Setting the queryMovie variable to the random movie.
+var questionMovieNames = getRandomMovies(movieArray, 4);                                                                                                        // Getting 4 random movies from the movieArray and adding them to a new array.
+var randomIndex = Math.floor(Math.random() * questionMovieNames.length);                                                                                        // Picking a random movie index from the new array.
+var queryMovie = questionMovieNames[randomIndex];                                                                                                               // Setting the queryMovie variable to the random movie.
 correctAnswer.push(queryMovie);                                                                                                                                 // Pushing the correct movie to the correctAnswer array.
 
 function getRandomMovies(array, count) {                                                                                                                        // Defining a function to randomise the movieArray.
-    var shuffledArray = array.sort(() => Math.random() - 0.5);                                                                                                // Shuffling the array with in random order.
+    var shuffledArray = array.sort(() => Math.random() - 0.5);                                                                                                  // Shuffling the array with in random order.
     return shuffledArray.slice(0, count);                                                                                                                       // Returning the newly shuffled array.
 };
 getRandomMovies(movieArray);                                                                                                                                    // Calling the getRandomMovies function and passing in the movieArray variable.
